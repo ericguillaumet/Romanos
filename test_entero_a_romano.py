0c4f461a7cc94c87617a8b7a49e17445a551c617
ericguillaumet <eguillaumet@protonmail.com>
2022-11-16 04:41:45 +0800

    ultimos cambios

--- test_entero_a_romano.py
+++ test_entero_a_romano.py
@@ -2,3 +2,6 @@ from main import entero_a_romano
 
 def test_entero_a_romano():
     assert entero_a_romano(336) == ['0000', '300', '30', '6']
+
+def test_entero_a_romano2():
+    assert entero_a_romano(336) == "CCCXXXVI"
