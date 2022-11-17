90eaee45fb1f5df654553f3fb45fa18e081568a8
ericguillaumet <eguillaumet@protonmail.com>
2022-11-17 11:11:48 +0800

    repasando videos katas 2

--- main.py
+++ main.py
@@ -16,35 +16,29 @@ f) 4.5 -> RomanNumberError("Debe ser un entero")
 class RomanNumberError(Exception):
     pass
 
-diccionario = {
-    'M': 1000,
-    'D': 500,
-    'C': 100,
-    'L': 50,
-    'X': 10,
-    'V': 5,
-    'I': 1
+milesimas = {
+    1000: "M", 2000: "MM", 3000: "MMM"
 }
 
 unidades = {
-    "I": 1, "II": 2, "III": 3,
-    "IV": 4, "V": 5, "VI": 6,
-    "VII": 7, "VIII": 8, "IX": 9
-
+    1: "I", 2: "II", 3: "III",
+    4: "IV", 5: "V", 6: "VI",
+    7: "VII", 8: "VIII", 9: "IX"
 }
 
 decenas = {
-    "X": 10, "XX": 20, "XXX": 30,
-    "XL": 40, "L": 50, "LX": 60,
-    "LXX": 70, "LXXX": 80, "XC": 90
-
+    10: "X", 20: "XX", 30: "XXX",
+    40: "XL", 50: "L", 60: "LX",
+    70: "LXX", 80: "LXXX", 90: "XC"
 }
 
 centenas = {
-    "C": 100, "CC": 200, "CCC": 300,
-    "CD": 400, "D": 500, "DC": 600,
-    "DCC": 700, "DCCC": 800, "CM": 900
+    100: "C", 200: "CC", 300: "CCC",
+    400: "CD", 500: "D", 600: "DC",
+    700: "DCC", 800: "DCCC", 900: "CM"
 }
+
+print("diccionario unidades ", unidades.get("I"))
 
 def entero_a_romano(num):
     lista = []
@@ -54,23 +48,11 @@ def entero_a_romano(num):
 
     lista = list(num)
     for i in range(len(lista)):
-        lista[i] = lista[i]+"0"*((len(lista)-1)-i)
+        lista[i] = lista[i] + "0" * ((len(lista)-1)-i)
 
     return lista
 
 print("Funcion en acción", entero_a_romano(336))
 
-
-for c,v in diccionario.items():
-    print(c + "-" + str(v))
-
-
-'''
-M → 1000
-D → 500
-C → 100
-L → 50
-X → 10
-V → 5
-I → 1
-'''
+for c,v in unidades.items():
+    print(str(c) + " - " + str(v))
